dbccf8903e7e166d8cf646e0bcf68c621281e66f
Joey Cos <joeycosentino24@gmail.com>
2022-11-11 18:36:13 -0500

    clock fully functional

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 setInterval(setClock, 1000)
 
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
 function setClock() {
     const currentDate = new Date();
     const setSeconds = currentDate.getSeconds() / 60;
     const setMinutes = (setSeconds + currentDate.getMinutes()) / 60;
-    const setHours = (setMinutes + currentDate.getHourss()) / 12;
+    const setHours = (setMinutes + currentDate.getHours()) / 12;
+    setRotation(secondHand, setSeconds);
+    setRotation(minuteHand, setMinutes);
+    setRotation(hourHand, setHours);
 }
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock();
